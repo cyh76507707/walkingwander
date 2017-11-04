da81e086a424f695e11a7d40b856badbcc929b25
Andrew Cho <cyh76507707@gmail.com>
2017-11-04 15:20:48 +0000

    basic setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require jquery.easing
+//= require bootstrap-sprockets
 //= require_tree .
